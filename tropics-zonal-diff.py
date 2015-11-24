e8df8b1a51e528612c53e7c8634c432c8f75180f
jws52 <j.w.smith@damtp.cam.ac.uk>
2015-11-24 16:33:04 +0000

    Update tropics-zonal-diff.py

--- tropics-zonal-diff.py
+++ tropics-zonal-diff.py
@@ -1,4 +1,3 @@
-#Code to obtain zonal mean and meridional mean across 10degS-10degN and difference between two datasets
 import iris
 import iris.analysis
 import numpy as np
@@ -28,12 +27,24 @@ print('New time co-ordinate range is for set 2: /n',q02.coord('t'))
 q_zonal1 = q01.collapsed('longitude', iris.analysis.MEAN)
 q_zonal2 = q02.collapsed('longitude', iris.analysis.MEAN)
 
-# Form the mean over the tropics, -10deg to +10deg
+# Form the mean over the tropics
 coord1 = q1.coord('latitude')
 coord2 = q2.coord('latitude')
 tropics1 = (coord1.points <=10) & (coord1.points >= -10)
 tropics2 = (coord2.points <=10) & (coord2.points >= -10)
 q_trop_zonal_01 = q_zonal1[:,:,tropics1]
 q_trop_zonal_02 = q_zonal2[:,:,tropics2]
-q_trop_zonal_01 = q_trop_zonal_01.collapse('latitude', iris.analysis.MEAN)
-q_trop_zonal_02 = q_trop_zonal_02.collapse('latitude', iris.analysis.MEAN)
+q_trop_zonal_01 = q_trop_zonal_01.collapsed('latitude', iris.analysis.MEAN)
+q_trop_zonal_02 = q_trop_zonal_02.collapsed('latitude', iris.analysis.MEAN)
+
+# Select the pressure level (70hPa for q, and 100hPa for T)
+q_final1 = q_trop_zonal_01.extract(iris.Constraint(Pressure=70))
+q_final2 = q_trop_zonal_02.extract(iris.Constraint(Pressure=70))
+q_final = q_final1 - q_final2
+
+# Plotting the difference as a function of time
+import matplotlib.pyplot as plt
+import iris.plot as iplt
+import iris.quickplot as qplot
+qplot.plot(q_final)
+plt.show()
