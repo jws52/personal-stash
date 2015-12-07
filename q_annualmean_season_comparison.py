010cf9177b54ca5249cb1d6d0eb8d1b1768843dc
jws52 <j.w.smith@damtp.cam.ac.uk>
2015-12-07 16:02:55 +0000

    Update q_annualmean_season_comparison.py

--- q_annualmean_season_comparison.py
+++ q_annualmean_season_comparison.py
@@ -2,6 +2,7 @@ import iris
 import iris.analysis
 import numpy as np
 import os.path
+import matplotlib.pyplot as plt
 
 # LOAD DATA
 filenames1 = () # Define the two filenames to work with
@@ -10,14 +11,14 @@ field_titles = ()
 variables = ['specific_humidity','air_temperature']
 variable = raw_input(["Which variable to use?", variables, "(please type explicitly"])
 response = "yes"
-#while response != "no":
-    filename2 = [aojeb, antie, antie, antie] #raw_input("Experiment ID for baseline run of comparison?")
-    filename1 = [aojed, antng, dlkxi, anvti] #raw_input("Experiment ID for modification run of comparison?")
-    field_title = ['ozone', 'radiative', 'ice microphysics', 'cirrus spreading'] #raw_input ("Title of comparison?")
+while response != "no":
+    filename2 = raw_input("Experiment ID for baseline run of comparison?")
+    filename1 = raw_input("Experiment ID for modification run of comparison?")
+    field_title = raw_input ("Title of comparison?")
     filenames1 += ('/group_workspaces/jasmin2/ukca/jsmith52/tq-selections/tq-selection-'+filename1+'.nc',)
     filenames2 += ('/group_workspaces/jasmin2/ukca/jsmith52/tq-selections/tq-selection-'+filename2+'.nc',)
     field_titles += (field_title,)
-    #response = raw_input("Another pair to compare? (yes/no)")
+    response = raw_input("Another pair to compare? (yes/no)")
 tropic_lats = iris.Constraint(latitude = lambda l: -10<=l<=10) # constrains loaded data to the tropical latitudes -10deg to +10deg
 
 # ANALYSIS
@@ -42,21 +43,21 @@ for i in range(len(filenames1)): # Loop over all comparison runs
     q02 = q2[t_range2,:,:,:]
     #print('New time co-ordinate range is for set 1: /n',q01.coord('t'))
     #print('New time co-ordinate range is for set 2: /n',q02.coord('t'))
-    # Form the zonal mean
-    q_zonal_01 = q01.collapsed('longitude', iris.analysis.MEAN)
-    q_zonal_02 = q02.collapsed('longitude', iris.analysis.MEAN)
-    # Form the mean over the tropics
-    q_trop_zonal_01 = q_zonal_01.collapsed('latitude', iris.analysis.MEAN)
-    q_trop_zonal_02 = q_zonal_02.collapsed('latitude', iris.analysis.MEAN)
-    # Select the pressure level (100hPa for q)
-    q_final1 = q_trop_zonal_01.extract(iris.Constraint(Pressure=100))
-    q_final2 = q_trop_zonal_02.extract(iris.Constraint(Pressure=100))
+    # Select the pressure level (100hPa for q, or T)
+    q_p_01 = q01.extract(iris.Constraint(Pressure=100))
+    q_p_02 = q02.extract(iris.Constraint(Pressure=100))
+        # Form the mean over the tropics
+    q_p_trop_1 = q_p_01.collapsed('latitude', iris.analysis.MEAN)
+    q_p_trop_2 = q_p_02.collapsed('latitude', iris.analysis.MEAN)
+        # Form the zonal mean
+    q_final1 = q_p_trop_1.collapsed('longitude', iris.analysis.MEAN)
+    q_final2 = q_p_trop_2.collapsed('longitude', iris.analysis.MEAN)
         # Extracting region in time suitable for comparison
     ## Can modify so that script takes maximum number of 12-month periods from q_01.coord('t').points
     t_start = 1999 #input("Starting year? Expect 1999.")
     t_end = 2008 #input("Endingyear? Expect 2008.")
+    # Time origin is 01-SEP-1988:00:00:00
     yr0 = (q_final1.coord('t').points >= (t_start-1-1988)*360+120) & (q_final1.coord('t').points < (t_end-1988)*360+120)
-    #print yr0
     
     # Calculation of difference in annual mean
     q_final = q_final1 - q_final2
@@ -67,7 +68,7 @@ for i in range(len(filenames1)): # Loop over all comparison runs
     print "Difference in annual mean is: ",diff_mean
     q_mean.append(diff_mean) 
     
-    # Calculation of difference in seasonal cycle amplitude
+    # Calculation of difference in seasonal cycle amplitude (zonally averaged)
     # Forming the climatological mean for t_start to t_end
     q_diff1 = q_final1[yr0]
     q_diff2 = q_final2[yr0]
@@ -76,26 +77,52 @@ for i in range(len(filenames1)): # Loop over all comparison runs
     # Finding amplitude of seasonal cycle
     iris.coord_categorisation.add_month(q_diff1, 't', name='climatological months')
     iris.coord_categorisation.add_month(q_diff2, 't', name='climatological months')
-    #print q_diff1
-    #print q_diff2
     climatological_mean1 = q_diff1.aggregated_by(['climatological months'], iris.analysis.MEAN)
     climatological_mean2 = q_diff2.aggregated_by(['climatological months'], iris.analysis.MEAN)
-    #print repr(climatological_mean1)
-    #print repr(climatological_mean2)
-    #climatological_mean1.coord('climatological months')
-    #climatological_mean2.coord('climatological months')
     seasonal_cycle1 = max(climatological_mean1.data)- min(climatological_mean1.data)
     seasonal_cycle2 = max(climatological_mean2.data)- min(climatological_mean2.data)
     # Taking difference in seasonal cycle
     diff_in_seasonal_cycle = (seasonal_cycle1-seasonal_cycle2)
     if variable == 'specific_humidity': diff_in_seasonal_cycle *= 1.608e6        # 1.608e6 is the units conversion from kg.kg^-1 to ppmv)
     print "Difference in seasonal cycle is: ", diff_in_seasonal_cycle
-    q_season.append(diff_in_seasonal_cycle) 
+    q_season.append(diff_in_seasonal_cycle)
+    
+    #Calculation of difference in seasonal cycle amplitude
+    # Forming the climatological mean for t_start to t_end
+    q_diff1 = q_p_01[yr0]
+    q_diff2 = q_p_02[yr0]
+    # Finding amplitude of seasonal cycle
+    iris.coord_categorisation.add_month(q_diff1, 't', name='climatological months')
+    iris.coord_categorisation.add_month(q_diff2, 't', name='climatological months')
+    climatological_mean1 = q_diff1.aggregated_by(['climatological months'], iris.analysis.MEAN)
+    climatological_mean2 = q_diff2.aggregated_by(['climatological months'], iris.analysis.MEAN)
+    cycle1_max = climatological_mean1.collapsed('t', iris.analysis.MAX)
+    cycle1_min = climatological_mean1.collapsed('t', iris.analysis.MIN)
+    seasonal_cycle1 = cycle1_max - cycle1_min
+    cycle2_max = climatological_mean2.collapsed('t', iris.analysis.MAX)
+    cycle2_min = climatological_mean2.collapsed('t', iris.analysis.MIN)
+    seasonal_cycle1 = cycle1_max - cycle1_min
+    seasonal_cycle2 = cycle2_max - cycle2_min
+            # Taking difference in seasonal cycle
+    diff_in_seasonal_cycle = seasonal_cycle1-seasonal_cycle2
+    if variable == 'specific_humidity': diff_in_seasonal_cycle *= 1.608e6        # 1.608e6 is the units conversion from kg.kg^-1 to ppmv)
+    print "Calculated seasonal difference at each latitude-longitude point"
+
+    # PLOTTING for point 2
+    #For annual mean
+    d_mean.data *= 1.608e6
+    plt.subplot(211)
+    plt.plot(d_mean.coord('longitude').points, d_mean.data)
+    plt.plot(d_mean.coord('longitude').points, [q_mean for x in d_mean.coord('longitude').points])
+    # For seasonal difference
+    plt.subplot(212)
+    plt.plot(d_mean.coord('longitude').points, d_mean.data)
+    plt.plot(d_mean.coord('longitude').points, [diff_in_seasonal_cycle.collapsed('latitude', iris.analysis.MEAN) for x in d_mean.coord('longitude').points])
+    
 print q_mean
 print q_season
 
 # PLOTTING for point 1
-import matplotlib.pyplot as plt
 plt.plot(q_mean[0],q_season[0], 'bo', q_mean[1],q_season[1], 'yo', q_mean[2],q_season[2], 'mo', q_mean[3],q_season[3], 'go')
 plt.legend(field_titles, loc=2)
 m, b = np.polyfit(q_mean, q_season, 1)
