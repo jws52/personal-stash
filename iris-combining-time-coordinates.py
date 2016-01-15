045c288e20614132dc8902b5e9a1575268c39fbe
jws52 <j.w.smith@damtp.cam.ac.uk>
2016-01-15 17:41:53 +0000

    Update iris-combining-time-coordinates.py

--- iris-combining-time-coordinates.py
+++ iris-combining-time-coordinates.py
@@ -5,13 +5,17 @@ import iris.coord_categorisation
 import numpy as np
 
 # Load the 
-cubew1 = iris.load('anmxa-partial-1w.nc')
-cubet1 = iris.load('anmxa-partial-1t.nc')
-cubeq1 = iris.load('anmxa-partial-1q.nc')
-cubew2 = iris.load('anmxa-partial-2w.nc')
-cubet2 = iris.load('anmxa-partial-2t.nc')
-cubeq2 = iris.load('anmxa-partial-2q.nc')
+cubew1 = iris.load_cube('anmxa-partial-1w.nc')
+cubet1 = iris.load_cube('anmxa-partial-1t.nc')
+cubeq1 = iris.load_cube('anmxa-partial-1q.nc')
+cubew2 = iris.load_cube('anmxa-partial-2w.nc')
+cubet2 = iris.load_cube('anmxa-partial-2t.nc')
+cubeq2 = iris.load_cube('anmxa-partial-2q.nc')
+month = iris.load('anmxa-missingmonth.nc')
 
+montht = month[0]
+monthq = month[1]
+monthw = month[2]
 
 ## For cubes above interpolate linearly between to create 't'= and save result to monthw, montht, monthq
 ## And check that the field names in the resulting cubes are the same as the original (except for the time value)
@@ -23,27 +27,31 @@ cubelistq1 = []
 cubelistw2 = []
 cubelistt2 = []
 cubelistq2 = []
-for i in range(len(cubew1)): cubelistw1 += cubew1[1][i,:,:,:]
-for i in range(len(cubet1)): cubelistt1 += cubet1[1][i,:,:,:]
-for i in range(len(cubeq1)): cubelistq1 += cubeq1[1][i,:,:,:]
-for i in range(len(cubew2)): cubelistw2 += cubew2[1][i+206,:,:,:]
-for i in range(len(cubet2)): cubelistt2 += cubet2[1][i+206,:,:,:]
-for i in range(len(cubeq2)): cubelistq2 += cubeq2[1][i+206,:,:,:]
-cubelistw1 = iris.CubeList(cubelistw1)
-cubelistt1 = iris.CubeList(cubelistt1)
-cubelistq1 = iris.CubeList(cubelistq1)
-cubelistw2 = iris.CubeList(cubelistw2)
-cubelistt2 = iris.CubeList(cubelistt2)
-cubelistq2 = iris.CubeList(cubelistq2)
+for i in range(len(cubew1.coord('t').points)): cubelistw1 += [cubew1[i,:,:,:]]
+for i in range(len(cubet1.coord('t').points)): cubelistt1 += [cubet1[i,:,:,:]]
+for i in range(len(cubeq1.coord('t').points)): cubelistq1 += [cubeq1[i,:,:,:]]
+for i in range(len(cubew2.coord('t').points)): cubelistw2 += [cubew2[i,:,:,:]]
+for i in range(len(cubet2.coord('t').points)): cubelistt2 += [cubet2[i,:,:,:]]
+for i in range(len(cubeq2.coord('t').points)): cubelistq2 += [cubeq2[i,:,:,:]]
+cubelistw1 = iris.cube.CubeList(cubelistw1)
+cubelistt1 = iris.cube.CubeList(cubelistt1)
+cubelistq1 = iris.cube.CubeList(cubelistq1)
+cubelistw2 = iris.cube.CubeList(cubelistw2)
+cubelistt2 = iris.cube.CubeList(cubelistt2)
+cubelistq2 = iris.cube.CubeList(cubelistq2)
 
 #Order and merge the related cubes
+cubelistw = []
+cubelistt = []
+cubelistq = []
 cubelistw = [cubelistw1, monthw, cubelistw2]
 cubelistt = [cubelistt1, montht, cubelistt2]
 cubelistq = [cubelistq1, monthq, cubelistq2]
-cubelistw = iris.CubeList(cubelistw)
-cubelistt = iris.CubeList(cubelistt)
-cubelistq = iris.CubeList(cubelistq)
+cubelistw = iris.cube.CubeList(cubelistw)
+cubelistt = iris.cube.CubeList(cubelistt)
+cubelistq = iris.cube.CubeList(cubelistq)
 mergew = cubelistw.merge_cube()
+#### Get an error here: "'CubeList' object has no attribute 'lazy_data'"
 merget = cubelistt.merge_cube()
 mergeq = cubelistq.merge_cube()
 
